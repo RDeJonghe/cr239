394efcc7ca656842bb15da862a5c5de652f332b4
RDeJonghe <rdejonghe@hotmail.com>
2022-10-12 16:44:07 -0500

    setup

--- contact_manager_node/public/javascripts/app.js
+++ contact_manager_node/public/javascripts/app.js
@@ -3,7 +3,6 @@ import View from '/javascripts/view.js';
 import Controller from '/javascripts/controller.js';
 
 /* eslint-disable no-new*/
-
 document.addEventListener('DOMContentLoaded', () => {
   new Controller(new Model(), new View());
 });
